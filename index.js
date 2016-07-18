892a3d0e995d473ae57d837c21b689ff1c5857f0
Vladimir Tyalo <bobtial@inbox.ru>
2016-07-18 15:27:27 +0400

    add index.js and prepare for deploying on heroku

---
+++ index.js
@@ -0,0 +1,35 @@
+(function () {
+  "use strict"
+
+  var express = require('express');
+  var app = express();
+
+  app.set("port", (process.env.PORT || 8080));
+  app.use(express.static(__dirname + "/views"));
+
+  app.set("views", __dirname + "/views");
+  app.set("views engine", "ejs");
+
+
+  app.get("/", function(req, res) {
+    res.render("index.html");
+  });
+
+  app.get("/index", function(req, res) {
+    res.redirect("/");
+  });
+
+  app.get("/catalog", function(req, res) {
+    res.render("catalog.html");
+  });
+
+  app.use(function(req, res) {
+    res.sendStatus(404);
+  });
+
+  app.listen(app.get("port"), function() {
+    console.log("Node app is running on port ", app.get("port"));
+
+  });
+
+})();
